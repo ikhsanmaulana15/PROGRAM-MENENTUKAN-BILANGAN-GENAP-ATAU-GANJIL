f83584102652ca641327d62dd22923b60460e1c6
IKHSAN MAULANA <45529723+ikhsanmaulana15@users.noreply.github.com>
2018-12-08 20:58:59 +0700

    Add files via upload

---
+++ bilganjil.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{int bilangan;
+cout << " Masukan Angka = ";
+cin>>bilangan;
+if (bilangan %2==0)
+    cout << " Bilangan Genap "<<endl;
+else
+    cout << " Bilangan Ganjil "<<endl;
+}
